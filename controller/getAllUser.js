3b3402e8e196a7e8ffdc8ef5ad861947a1bbcf6b
flutter-abhi <flutter.abhishek@gmail.com>
2024-08-11 01:05:29 +0530

    history and recent transfer created

--- controller/getAllUser.js
+++ controller/getAllUser.js
@@ -1,18 +1,21 @@
+
 const User = require("../schema/User");
 
 exports.getAllContact = async (req, res) => {
+
     try {
-        
+
         const users = await User.find({ type: "user" }).select('-password -walletBalance');
-        
+
         res.status(200).json({
-            success: true,
+            succsees: true,
             data: users
-        });
+        })
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
-            message: "server side error"
-        });
+            message: "server side error "
+        })
     }
 }
